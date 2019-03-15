aec4948398742ff93e8ecca20f7a61486f0928c3
admin <admin@ANSHULAGARWAL>
2019-03-15 18:31:06 +0530

    ctagrid paging work

--- src/app/ctagrid/ctagrid.component.ts
+++ src/app/ctagrid/ctagrid.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import { map } from 'rxjs/operators';
 import { beer } from './ctagrid.model';
+import { timingSafeEqual } from 'crypto';
 
 @Component({
   selector: 'app-ctagrid',
@@ -10,48 +11,57 @@ import { beer } from './ctagrid.model';
 })
 export class CTAGridComponent implements OnInit {
   beerList: beer[];
-  abv: number = 1;
+  abv: number = 0;
   page: number = 1;
   perPageNumber: number = 8;
   nextButtonDisable: boolean = false;
-  prevButtonDisable: boolean = true;
-  abv_lt:boolean = false;
-  basdf={
-    name:"sdas"
-  }
-
+  abv_lt: boolean = false;
+  filter: string = "";
+  baseUrl: string = "https://api.punkapi.com/v2/beers";
   constructor(private http: Http) { }
 
   ngOnInit() {
     this.getBeersList();
   }
 
+  search = function () {
+    if (!this.abv) {
+      this.abv = 0;
+    }
+
+    if (!this.abv_lt) {
+      this.filter = "&abv_gt=" + this.abv;
+    } else {
+      this.filter = "&abv_lt=" + this.abv;
+    }
+
+    this.getBeersList();
+
+  }
+
   getBeersList = function () {
-    var url = "https://api.punkapi.com/v2/beers?page=" + this.page + "&per_page=" + this.perPageNumber;
-    this.http.get(url).pipe(map((response) => response.json()))
-      .subscribe((data) => {
-        console.log(data);
-        this.beerList = data;
-        if (this.beerList.length < this.perPageNumber) {
-          this.nextButtonDisable = true;
-        }
-      });
+    if (this.page > 0 && !this.nextButtonDisable) {
+      var url = this.baseUrl + "?page=" + this.page + "&per_page=" + this.perPageNumber + this.filter;
+
+      this.http.get(url).pipe(map((response) => response.json()))
+        .subscribe((data) => {
+          console.log(data);
+          this.beerList = data;
+          if (this.beerList.length < this.perPageNumber) {
+            this.nextButtonDisable = true;
+          }
+        });
+    }
   }
 
   nextList = function () {
     this.page += 1;
-    if (this.page > 1) {
-      this.prevButtonDisable = false;
-    }
     this.getBeersList();
   }
 
   prevList = function () {
     this.page -= 1;
-    if (this.page == 1) {
-      this.prevButtonDisable = true;
-    }
-
+    this.nextButtonDisable = false;
     this.getBeersList();
   }
 
